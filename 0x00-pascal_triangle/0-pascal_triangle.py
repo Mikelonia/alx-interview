e4f94e158ea08450ab9c4539a85b1c73a1ea32fa
Mikelonia <onajitemike4real@gmail.com>
2023-10-06 23:05:03 -0700

    Task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''A module for working with Pascal's triangle.
+'''
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
+
+def print_triangle(triangle):
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+if __name__ == "__main__":
+    n = 5
+    result = pascal_triangle(n)
+    print_triangle(result)
+

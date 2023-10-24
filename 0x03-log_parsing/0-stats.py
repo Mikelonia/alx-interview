7f46dda9d0b08cecf6918d651e095103a0a70191
Mikelonia <onajitemike4real@gmail.com>
2023-10-24 05:14:30 +0000

    Removal of trailing whitespace

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,88 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics:. By Okpako Michael
+'''A script for parsing HTTP request logs. By Okpako Michael
 '''
-import sys
 import re
 
-def process_line(line):
-    # Define the regular expression pattern for matching the input format
-    pattern = re.compile(r'^(\d+\.\d+\.\d+\.\d+) - \[.*\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)$')
 
-    match = pattern.match(line)
-    if match:
-        ip_address, status_code, file_size = match.groups()
-        return int(status_code), int(file_size)
-    else:
-        return None, None
+def extract_input(input_line):
+    '''Extracts sections of a line of an HTTP request log.
+    '''
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
-def print_metrics(total_size, status_counts):
-    print(f"Total file size: {total_size}")
-    for code in sorted(status_counts):
-        print(f"{code}: {status_counts[code]}")
 
-def main():
-    total_size = 0
-    status_counts = {}
+def print_statistics(total_file_size, status_codes_stats):
+    '''Prints the accumulated statistics of the HTTP request log.
+    '''
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
+
+def update_metrics(line, total_file_size, status_codes_stats):
+    '''Updates the metrics from a given HTTP request log.
+
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+
+    Returns:
+        int: The new total file size.
+    '''
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
+
+
+def run():
+    '''Starts the log parser.
+    '''
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
     try:
-        for line_number, line in enumerate(sys.stdin, start=1):
-            status_code, file_size = process_line(line.strip())
-            
-            if status_code is not None and file_size is not None:
-                total_size += file_size
-                status_counts[status_code] = status_counts.get(status_code, 0) + 1
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
 
-            if line_number % 10 == 0:
-                print_metrics(total_size, status_counts)
 
-    except KeyboardInterrupt:
-        print_metrics(total_size, status_counts)
-
-if __name__ == "__main__":
-    main()
-
+if __name__ == '__main__':
+    run()
